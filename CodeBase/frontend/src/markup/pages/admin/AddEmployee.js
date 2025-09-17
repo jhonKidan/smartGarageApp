f1002d265a403e2ddd15b39e276e2e20f5dc6339
jhonKidan <jhonkidan.777@gmail.com>
2025-09-17 19:08:09 +0300

    added some update on dashboard

--- CodeBase/frontend/src/markup/pages/admin/AddEmployee.js
+++ CodeBase/frontend/src/markup/pages/admin/AddEmployee.js
@@ -1,10 +1,17 @@
-import React from 'react';
-// Import the AddEmployeeForm component 
+import React, { useState } from 'react';
 import AddEmployeeForm from '../../components/Admin/AddEmployeeForm/AddEmployeeForm';
-// Import the AdminMenu component 
 import AdminMenu from '../../components/Admin/AdminMenu/AdminMenu';
 
-function AddEmployee(props) {
+function AddEmployee({ onEmployeeAdded }) {
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+  const handleEmployeeAdded = () => {
+    setRefreshTrigger(prev => prev + 1); // Increment to trigger refresh
+    if (onEmployeeAdded) {
+      onEmployeeAdded(); // Notify parent component (if needed)
+    }
+  };
+
   return (
     <div>
       <div className="container-fluid admin-pages">
@@ -13,7 +20,7 @@ function AddEmployee(props) {
             <AdminMenu />
           </div>
           <div className="col-md-9 admin-right-side">
-            <AddEmployeeForm />
+            <AddEmployeeForm onEmployeeAdded={handleEmployeeAdded} refreshTrigger={refreshTrigger} />
           </div>
         </div>
       </div>
